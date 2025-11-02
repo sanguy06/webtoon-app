624fa8d71477d1f9de625421139d063b9bd1b696
sanguy06 <103820641+sanguy06@users.noreply.github.com>
2025-11-01 23:25:05 -0700

    fixing issues with frontend

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -8,9 +8,9 @@ import "./SearchBar.css";
 
 export default function SearchBar({setResults}) {
     const [input, setInput] = useState("");
-
+    const API_URL = process.env.REACT_APP_API_URl
     const fetchData = async (value) => {
-        await axios.get(`http://localhost:5555/search-webtoons?webtoonTitle=${value}`)
+        await axios.get(`${API_URL}/search-webtoons?webtoonTitle=${value}`)
                     .then((res)=>{
                         setResults(res.data);
          })
